0e6ecca94ebe5fc5d7dbea0e252fbea0db4024f8
Yuri Blanc <yuxblank@gmail.com>
2018-03-21 20:56:35 +0100

    components update + .htaccess for apache

--- install/SQL/example_dml.sql
+++ install/SQL/example_dml.sql
@@ -1,5 +1,4 @@
-INSERT INTO user (id, username, email, password, date_created, date_updated, status) VALUES (1, 'info@yuriblanc.it', 'info@yuriblanc.it', '$2y$10$Zw92VDDlCs1YcIqDmR8YYe7CcAXyIa5nJAhY2uqjZ1CuzydrD6cFq', '2017-12-28 23:27:31', null, '1');
-INSERT INTO userrole (id, title, level, date_created, date_updated, status) VALUES (1, 'superuser', 0, '2017-12-28 23:30:32', null, '1');
-INSERT INTO user_roles (user_id, user_role_id) VALUES (1, 1);
-
 INSERT INTO phackpcms.user (username, email, password, date_created, date_updated, status) VALUES ('info@yuriblanc.it', 'info@yuriblanc.it', '$2y$10$Zw92VDDlCs1YcIqDmR8YYe7CcAXyIa5nJAhY2uqjZ1CuzydrD6cFq', null, null, '1');
+INSERT INTO phackpcms.userrole (id, title, level, date_created, date_updated, status) VALUES (NULL , 'superuser', 0, null, null, '1');
+INSERT INTO phackpcms.user_roles (user_id, user_role_id) VALUES (1, 1);
+INSERT INTO phackpcms.oauth_client (id, identifier, name, redirect_uri, date_created, date_updated, status) VALUES (1, 'TEST_CLIENT', 'TEST_CLIENT', '/', '2018-01-27 23:07:12', null, '1');
